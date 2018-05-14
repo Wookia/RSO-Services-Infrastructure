8a43db0d4d8a9f27a62ec0820ec2492c77a6e381
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2018-05-14 19:43:18 +0200

    press any key to continue

--- start.sh
+++ start.sh
@@ -5,3 +5,4 @@ docker-compose build
 docker-compose up -d
 
 echo Assuming there have been no errors, Paragon services running on docker host, port 8000
+read -p "Press any key..."./
--- start_local.sh
+++ start_local.sh
@@ -5,3 +5,4 @@ docker-compose -f docker-compose.dev.yml build
 docker-compose -f docker-compose.dev.yml up -d
 
 echo Assuming there have been no errors, Paragon services running on docker host, port 8000
+read -p "Press any key..."
